b50f1088934d0a82e8ffc37edfe3c00f68f67028
sfivaz <31249205+sfivaz@users.noreply.github.com>
2019-04-23 17:39:24 +0200

    Add files via upload

--- routes.php
+++ routes.php
@@ -1,11 +1,22 @@
 <?php
 require_once("site/global.php");
 
+//check auth
+session_start();
+
+//get the uri
 $request = $_SERVER["REQUEST_URI"];
 
 $route = new Router($request);
 
-$route->redirect("", "AccountController", "findAll");
-$route->redirect("account", "AccountController", "find");
-$route->redirect("categories", "CategoryController", "findAll");
-$route->get("server", "site/php/services/server.php");
+$route->get("/register", "AuthController", "register");
+$route->get("/login", "AuthController", "login");
+$route->get("/", "HomeController", "index");
+$route->get("/home", "HomeController", "index");
+$route->getAuth("/logout", "AuthController", "logout");
+$route->getAuth("/accounts", "AccountController", "index");
+$route->getAuth("/account", "AccountController", "get");
+$route->getAuth("/categories", "CategoryController", "index");
+
+$route->postGuest("/server", "User", "insert");
+$route->post("/server");
